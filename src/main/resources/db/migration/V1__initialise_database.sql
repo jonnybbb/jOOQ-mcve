2c932022bc876eeb56eb5a13ee8643560b28b1ca
Johannes Bühler <johannes@gradle.com>
2020-11-30 22:28:07 +0100

    working code with jooq 3.13.6

--- src/main/resources/db/migration/V1__initialise_database.sql
+++ src/main/resources/db/migration/V1__initialise_database.sql
@@ -3,8 +3,12 @@ DROP SCHEMA IF EXISTS mcve CASCADE;
 CREATE SCHEMA mcve;
 
 CREATE TABLE mcve.test (
-  id    INT NOT NULL AUTO_INCREMENT,
-  value INT,
-  
-  CONSTRAINT pk_test PRIMARY KEY (id) 
+  id    SERIAL PRIMARY KEY,
+  value INT
 );
+
+CREATE OR REPLACE FUNCTION mcve.foo(i integer) RETURNS integer AS $$
+BEGIN
+RETURN i + 1;
+END;
+$$ LANGUAGE plpgsql;
--- src/test/java/org/jooq/mcve/test/java/JavaTest.java
+++ src/test/java/org/jooq/mcve/test/java/JavaTest.java
@@ -1,23 +1,15 @@
 package org.jooq.mcve.test.java;
 
-import org.jooq.mcve.java.tables.records.TestRecord;
+import org.jooq.mcve.java.Routines;
 import org.junit.Test;
 
-import static org.jooq.mcve.java.Tables.TEST;
 import static org.junit.Assert.assertEquals;
 
 public class JavaTest extends AbstractTest {
 
     @Test
     public void mcveTest() {
-        TestRecord result =
-        ctx.insertInto(TEST)
-           .columns(TEST.VALUE)
-           .values(42)
-           .returning(TEST.ID)
-           .fetchOne();
-
-        result.refresh();
-        assertEquals(42, (int) result.getValue());
+        Integer foo = Routines.foo(ctx.configuration(), 1);
+        assertEquals(2, foo.intValue());
     }
 }
